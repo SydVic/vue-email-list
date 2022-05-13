e27620e9cef6182b6cb04267f11f46d60e0e35b1
SydVic <fabrizio.bocca27@gmail.com>
2022-05-13 15:25:43 +0200

    aggiunta stampa in html della lista di email

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 const app = new Vue (
     {
-        root: "#root",
+        el: "#root",
         data: {
-
+            emailArray: [],
+            arrayLength: 10,
+            email: "",
         },
-        methods: {
-
+        methods: { 
+            getEmails() {
+                for (let i = 0; i < this.arrayLength; i++) {
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) => {
+                        this.emailArray.push(resp.data.response);
+                    });
+                }
+            }
         },
-    },
+        mounted() {
+            this.getEmails();
+        }
+    }
 );
