98dcbac954eae7cf673da2859c509c9845e40467
SydVic <fabrizio.bocca27@gmail.com>
2022-05-16 09:59:36 +0200

    aggiunto v-if e correzione

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const app = new Vue (
     {
         el: "#root",
         data: {
-            emailArray: [],
+            emailsArray: [],
             arrayLength: 10,
             email: "",
         },
@@ -18,6 +18,29 @@ const app = new Vue (
         },
         mounted() {
             this.getEmails();
-        }
+        },
     }
 );
+
+// correzione
+// const app = new Vue (
+//     {
+//         el: "#root",
+//         data: {
+//             emailsArray: [],
+//         },
+//         methods: { 
+            
+//         },
+//         created() {
+//             for (let i = 0; i < 10; i++) {
+//                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//                 .then((resp) => {
+//                     const newEmail = resp.data.response;
+//                     this.emailsArray.push(newEmail);
+//                 });
+//             }
+//         }
+//     }
+// );
+// ciclo while non va bene, per la questione asincronicità. Prima che arrivi la risposta vengono eseguiti X cicli
